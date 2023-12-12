dc519bd50b82166b06e0dadc093d1c67df69df2e
Ashby Issac <ashbyissac8@gmail.com>
2023-12-12 21:52:08 +0530

    Prototype-Phase-1 changes

---
+++ FPSPrototype/Source/FPSPrototype/FPSPrototypeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSPrototypeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSPROTOTYPE_API AFPSPrototypeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
